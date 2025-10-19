bcdf8e2b13d43b1c37a749e11a6dd3b68479f0ee
jkunkel299 <jmk299pitt@gmail.com>
2025-10-19 15:08:29 -0400

    remove test from vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -1,30 +1,7 @@
-/// <reference types="vitest" />
-/// <reference types="vite/client" />
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  test: {
-    environment: "jsdom",         // needed for DOM testing
-    globals: true,                // allows using `describe`, `it`, `expect` globally
-    // setupFiles: "./src/setupTests.ts",  // optional: test setup file
-    css: true,                    // allow importing CSS in tests
-    coverage: {
-      reporter: ["text", "json", "html"],  // generates coverage reports
-    },
-    exclude: [
-      "node_modules",
-      "dist",
-      "e2e",
-      "playwright.config.*",
-    ],
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
 })
