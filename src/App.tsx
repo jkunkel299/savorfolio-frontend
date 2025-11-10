540d190e6683e39170332e21b19bf42b88989bf3
jkunkel299 <jmk299pitt@gmail.com>
2025-11-10 09:22:11 -0500

    moved router into a separate component, testing new playwright github workflow

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,7 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Layout from "./components/Layouts/Layout";
-import RecipeSearchPage from "./pages/RecipeSearchPage";
-import { AddRecipePage } from "./pages/AddRecipePage";
-import HomePage from "./pages/HomePage";
-import RecipeConfirmedPage from "./pages/RecipeConfirmedPage";
-import { ViewRecipePage } from "./pages/ViewRecipePage";
-import { ChooseInputPage } from "./pages/ChooseInputPage";
+
 import { GlobalStyles, ThemeProvider } from "@mui/material";
 import theme from "./themes/theme";
+import AppRouter from "./router/AppRouter";
 
 
 const App: React.FC = () => {
@@ -39,16 +33,7 @@ const App: React.FC = () => {
         }}
     />
     <ThemeProvider theme={theme}>
-    <Router>
-      <Routes>
-        <Route path="/" element={<Layout><HomePage /></Layout>} />
-        <Route path="/search" element={<Layout><RecipeSearchPage /></Layout>} />
-        <Route path="/add-input" element={<Layout><ChooseInputPage /></Layout>} />
-        <Route path="/add" element={<Layout><AddRecipePage /></Layout>} />
-        <Route path="/confirmed" element={<Layout><RecipeConfirmedPage /></Layout>} />
-        <Route path="/view" element={<Layout><ViewRecipePage /></Layout>} />
-      </Routes>
-    </Router>
+        <AppRouter />
     </ThemeProvider>
     </>
     
---
+++ src/router/AppRouter.tsx
@@ -0,0 +1,23 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Layout from "../components/Layouts/Layout";
+import RecipeSearchPage from "../pages/RecipeSearchPage";
+import { AddRecipePage } from "../pages/AddRecipePage";
+import HomePage from "../pages/HomePage";
+import RecipeConfirmedPage from "../pages/RecipeConfirmedPage";
+import { ViewRecipePage } from "../pages/ViewRecipePage";
+import { ChooseInputPage } from "../pages/ChooseInputPage";
+
+const AppRouter: React.FC = () => (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Layout><HomePage /></Layout>} />
+        <Route path="/search" element={<Layout><RecipeSearchPage /></Layout>} />
+        <Route path="/add-input" element={<Layout><ChooseInputPage /></Layout>} />
+        <Route path="/add" element={<Layout><AddRecipePage /></Layout>} />
+        <Route path="/confirmed" element={<Layout><RecipeConfirmedPage /></Layout>} />
+        <Route path="/view" element={<Layout><ViewRecipePage /></Layout>} />
+      </Routes>
+    </Router>
+);
+
+export default AppRouter;
