8b968e7d4e053ec95669f7c780eb4de0e2fbb42f
jkunkel299 <jmk299pitt@gmail.com>
2025-11-27 07:41:54 -0500

    scrap user/auth for immediate functionality

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,90 +1,90 @@
-import Box from "@mui/material/Box";
-import Button from "@mui/material/Button";
-import FormGroup from "@mui/material/FormGroup";
-import OutlinedInput from "@mui/material/OutlinedInput";
-import Typography from "@mui/material/Typography";
-import { useState } from "react";
-import type { RootState } from "../react-redux/store";
-import { Link, useNavigate } from "react-router-dom";
-import { loginUser } from "../react-redux/slices/authSlice";
-import { useAppDispatch, useAppSelector } from "../react-redux/hooks";
-import { colors } from "../themes/colors";
-import type { UserLoginDTO } from "../types";
+// import Box from "@mui/material/Box";
+// import Button from "@mui/material/Button";
+// import FormGroup from "@mui/material/FormGroup";
+// import OutlinedInput from "@mui/material/OutlinedInput";
+// import Typography from "@mui/material/Typography";
+// import { useState } from "react";
+// import type { RootState } from "../react-redux/store";
+// import { Link, useNavigate } from "react-router-dom";
+// // import { loginUser } from "../react-redux/slices/authSlice";
+// import { useAppDispatch, useAppSelector } from "../react-redux/hooks";
+// import { colors } from "../themes/colors";
+// import type { UserLoginDTO } from "../types";
 
-export default function LoginPage() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+// export default function LoginPage() {
+//   const [email, setEmail] = useState("");
+//   const [password, setPassword] = useState("");
 
-  const dispatch = useAppDispatch();
-  const navigate = useNavigate();
+//   const dispatch = useAppDispatch();
+//   const navigate = useNavigate();
 
-  const { loading, error } = useAppSelector((state: RootState) => state.auth);
+//   const { loading, error } = useAppSelector((state: RootState) => state.auth);
 
-  const handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault();
+//   const handleSubmit = async (e: React.FormEvent) => {
+//     e.preventDefault();
 
-    const userLogin: UserLoginDTO = {
-      email: email,
-      password: password,
-    };
-    try {
-      await dispatch(loginUser(userLogin)).unwrap();
-      console.log("Login successful");
-      navigate("/");
-    } catch (err: unknown) {
-      console.error("Login failed:", err);
-    }
-  };
+//     const userLogin: UserLoginDTO = {
+//       email: email,
+//       password: password,
+//     };
+//     try {
+//       await dispatch(loginUser(userLogin)).unwrap();
+//       console.log("Login successful");
+//       navigate("/");
+//     } catch (err: unknown) {
+//       console.error("Login failed:", err);
+//     }
+//   };
 
-  return (
-    <Box
-      component="form"
-      onSubmit={handleSubmit}
-      display="flex"
-      justifyContent="center"
-      alignItems="center"
-      width="100%"
-      flexDirection="column"
-      sx={{ gap: 2 }}
-    >
-      <Box display="flex" flexDirection="column" gap={3}>
-        <Typography variant="h4" textAlign="center" gutterBottom>
-          Log In
-        </Typography>
+//   return (
+//     <Box
+//       component="form"
+//       onSubmit={handleSubmit}
+//       display="flex"
+//       justifyContent="center"
+//       alignItems="center"
+//       width="100%"
+//       flexDirection="column"
+//       sx={{ gap: 2 }}
+//     >
+//       <Box display="flex" flexDirection="column" gap={3}>
+//         <Typography variant="h4" textAlign="center" gutterBottom>
+//           Log In
+//         </Typography>
 
-        {/* email */}
-        <FormGroup>
-          <Typography>Email</Typography>
-          <OutlinedInput
-            placeholder="Email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </FormGroup>
+//         {/* email */}
+//         <FormGroup>
+//           <Typography>Email</Typography>
+//           <OutlinedInput
+//             placeholder="Email"
+//             value={email}
+//             onChange={(e) => setEmail(e.target.value)}
+//           />
+//         </FormGroup>
 
-        {/* password */}
-        <FormGroup>
-          <Typography>Password</Typography>
-          <OutlinedInput
-            placeholder="Password"
-            value={password}
-            type="password"
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </FormGroup>
+//         {/* password */}
+//         <FormGroup>
+//           <Typography>Password</Typography>
+//           <OutlinedInput
+//             placeholder="Password"
+//             value={password}
+//             type="password"
+//             onChange={(e) => setPassword(e.target.value)}
+//           />
+//         </FormGroup>
 
-        <Button type="submit" disabled={loading} variant="contained">
-          {loading ? "Logging in..." : "Log In"}
-        </Button>
+//         <Button type="submit" disabled={loading} variant="contained">
+//           {loading ? "Logging in..." : "Log In"}
+//         </Button>
 
-        {error && <Typography>{error}</Typography>}
+//         {error && <Typography>{error}</Typography>}
 
-        <Button color="inherit" component={Link} to="/auth/register">
-          <Typography color={colors.textPrimary}>
-            New user? Register here!
-          </Typography>
-        </Button>
-      </Box>
-    </Box>
-  );
-}
+//         <Button color="inherit" component={Link} to="/auth/register">
+//           <Typography color={colors.textPrimary}>
+//             New user? Register here!
+//           </Typography>
+//         </Button>
+//       </Box>
+//     </Box>
+//   );
+// }
--- src/react-redux/slices/authSlice.ts
+++ src/react-redux/slices/authSlice.ts
@@ -1,135 +1,135 @@
-import {
-  createAsyncThunk,
-  createSlice,
-  type PayloadAction,
-} from "@reduxjs/toolkit";
-import type { AuthReponse, User, UserLoginDTO } from "../../types";
-import axiosClient, { setAuthToken } from "../../api/axiosClient";
-import type { AppDispatch } from "../store";
-import { jwtDecode } from "jwt-decode";
+// import {
+//   createAsyncThunk,
+//   createSlice,
+//   type PayloadAction,
+// } from "@reduxjs/toolkit";
+// import type { AuthReponse, User, UserLoginDTO } from "../../types";
+// import axiosClient, { setAuthToken } from "../../api/axiosClient";
+// import type { AppDispatch } from "../store";
+// import { jwtDecode } from "jwt-decode";
 
-interface AuthState {
-  user: User | null;
-  token: string | null;
-  loading: boolean;
-  error: string | null;
-}
+// interface AuthState {
+//   user: User | null;
+//   token: string | null;
+//   loading: boolean;
+//   error: string | null;
+// }
 
-interface DecodedToken {
-  id: string;
-  email: string;
-  exp: number;
-  iss?: string;
-  aud?: string;
-}
+// interface DecodedToken {
+//   id: string;
+//   email: string;
+//   exp: number;
+//   iss?: string;
+//   aud?: string;
+// }
 
-const initialState: AuthState = {
-  user: null,
-  token: localStorage.getItem("token"),
-  loading: false,
-  error: null,
-};
+// const initialState: AuthState = {
+//   user: null,
+//   token: localStorage.getItem("token"),
+//   loading: false,
+//   error: null,
+// };
 
-// Thunks
-export const registerUser = createAsyncThunk<
-  AuthReponse,
-  UserLoginDTO,
-  { dispatch: AppDispatch }
->("auth/register",
-  async (credentials: UserLoginDTO, { rejectWithValue }) => {
-    try {
-      const response = await axiosClient.post<AuthReponse>(
-        "api/auth/register",
-        credentials
-      );
-      return response.data;
-    } catch {
-      return rejectWithValue("Registration failed");
-    }
-  }
-);
+// // Thunks
+// export const registerUser = createAsyncThunk<
+//   AuthReponse,
+//   UserLoginDTO,
+//   { dispatch: AppDispatch }
+// >("auth/register",
+//   async (credentials: UserLoginDTO, { rejectWithValue }) => {
+//     try {
+//       const response = await axiosClient.post<AuthReponse>(
+//         "api/auth/register",
+//         credentials
+//       );
+//       return response.data;
+//     } catch {
+//       return rejectWithValue("Registration failed");
+//     }
+//   }
+// );
 
-export const loginUser = createAsyncThunk<
-  AuthReponse,
-  UserLoginDTO,
-  { dispatch: AppDispatch }
->("auth/loginUser", async (credentials: UserLoginDTO, { rejectWithValue }) => {
-  try {
-    const response = await axiosClient.post<AuthReponse>(
-      "api/auth/login",
-      credentials
-    );
-    return response.data;
-  } catch {
-    return rejectWithValue("Login failed");
-  }
-});
+// export const loginUser = createAsyncThunk<
+//   AuthReponse,
+//   UserLoginDTO,
+//   { dispatch: AppDispatch }
+// >("auth/loginUser", async (credentials: UserLoginDTO, { rejectWithValue }) => {
+//   try {
+//     const response = await axiosClient.post<AuthReponse>(
+//       "api/auth/login",
+//       credentials
+//     );
+//     return response.data;
+//   } catch {
+//     return rejectWithValue("Login failed");
+//   }
+// });
 
-export const logoutUser = createAsyncThunk("auth/logoutUser", async () => {
-  localStorage.removeItem("token");
-  setAuthToken(null);
-});
+// export const logoutUser = createAsyncThunk("auth/logoutUser", async () => {
+//   localStorage.removeItem("token");
+//   setAuthToken(null);
+// });
 
-// Slice
-const authSlice = createSlice({
-  name: "auth",
-  initialState,
-  reducers: {
-    hydrateFromLocalStorage(state) {
-      const token = localStorage.getItem("token");
+// // Slice
+// const authSlice = createSlice({
+//   name: "auth",
+//   initialState,
+//   reducers: {
+//     hydrateFromLocalStorage(state) {
+//       const token = localStorage.getItem("token");
 
-      if (token) {
-        state.token = token;
+//       if (token) {
+//         state.token = token;
 
-        try {
-          const decoded = jwtDecode<DecodedToken>(token);
-          state.user = {
-            id: +decoded.id,
-            email: decoded.email,
-          };
-        } catch {
-          console.error("Invalid token in storage");
-          localStorage.removeItem("token");
-        }
-      }
-    },
-  },
+//         try {
+//           const decoded = jwtDecode<DecodedToken>(token);
+//           state.user = {
+//             id: +decoded.id,
+//             email: decoded.email,
+//           };
+//         } catch {
+//           console.error("Invalid token in storage");
+//           localStorage.removeItem("token");
+//         }
+//       }
+//     },
+//   },
 
-  extraReducers: (builder) => {
-    builder
-      .addCase(loginUser.pending, (state) => {
-        state.loading = true;
-        state.error = null;
-      })
-      .addCase(
-        loginUser.fulfilled,
-        (state, action: PayloadAction<AuthReponse>) => {
-          const token = action.payload.token;
-          state.loading = false;
-          const decoded = jwtDecode<DecodedToken>(token);
-          state.user = {
-            id: +decoded.id,
-            email: decoded.email,
-          };
-          state.token = token;
+//   extraReducers: (builder) => {
+//     builder
+//       .addCase(loginUser.pending, (state) => {
+//         state.loading = true;
+//         state.error = null;
+//       })
+//       .addCase(
+//         loginUser.fulfilled,
+//         (state, action: PayloadAction<AuthReponse>) => {
+//           const token = action.payload.token;
+//           state.loading = false;
+//           const decoded = jwtDecode<DecodedToken>(token);
+//           state.user = {
+//             id: +decoded.id,
+//             email: decoded.email,
+//           };
+//           state.token = token;
 
-          localStorage.setItem("token", token);
-          setAuthToken(token);
-        }
-      )
-      .addCase(loginUser.rejected, (state, action) => {
-        state.loading = false;
-        state.error = action.payload as string;
-      })
-      .addCase(logoutUser.fulfilled, (state) => {
-        state.user = null;
-        state.token = null;
-        state.error = null;
+//           localStorage.setItem("token", token);
+//           setAuthToken(token);
+//         }
+//       )
+//       .addCase(loginUser.rejected, (state, action) => {
+//         state.loading = false;
+//         state.error = action.payload as string;
+//       })
+//       .addCase(logoutUser.fulfilled, (state) => {
+//         state.user = null;
+//         state.token = null;
+//         state.error = null;
 
-        setAuthToken(null);
-      });
-  },
-});
+//         setAuthToken(null);
+//       });
+//   },
+// });
 
-export const { hydrateFromLocalStorage } = authSlice.actions;
-export default authSlice.reducer;
+// export const { hydrateFromLocalStorage } = authSlice.actions;
+// export default authSlice.reducer;
